fbf0a2365cc5fca39cc8a9880fa349660255039a
sisypheanbreeze <safyque@gmail.com>
2026-07-26 15:30:18 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function favoriteMovie() {
+
+    document.getElementById("favorite").innerHTML =
+    "My favorite example of this trope is <strong>Hannibal</strong>, because Will Graham and Hannibal Lecter are nearly identical people separated only by one irreconcilable difference: morality.";
+
+}
