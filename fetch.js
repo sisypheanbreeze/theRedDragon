eef5497ceeadb2a8a52ddf3a40a87d9f86c4cf38
sisypheanbreeze <safyque@gmail.com>
2026-07-26 19:33:07 -0400

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,30 @@
+fetch("movies.json")
+.then(response => response.json())
+.then(data => {
+
+    let table =
+    "<table id='jsonTable'>" +
+    "<tr>" +
+    "<th>Film</th>" +
+    "<th>Main Characters</th>" +
+    "<th>Irreconcilable Difference</th>" +
+    "<th>Release Year</th>" +
+    "</tr>";
+
+    data.forEach(function(movie){
+
+        table +=
+        "<tr>" +
+        "<td>" + movie.title + "</td>" +
+        "<td>" + movie.protagonists + "</td>" +
+        "<td>" + movie.difference + "</td>" +
+        "<td>" + movie.release + "</td>" +
+        "</tr>";
+
+    });
+
+    table += "</table>";
+
+    document.getElementById("movieData").innerHTML = table;
+
+});
